e08dfcf2d3dd60df656fbead35fb9c932c595aa5
elenaJO <ele.lucero1993@gmail.com>
2017-10-09 22:16:54 -0500

    Practica

---
+++ app.js
@@ -0,0 +1,5 @@
+var age=parseInt(prompt("cual es tu edad"));
+//convertiendo edad a segndos
+var ageInSecond=age*365*24*60*60;
+console.log(ageInSecond);
+document.write("tu edad es "+age+" y en segundos es "+ageInSecond);
